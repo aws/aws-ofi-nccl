adc676faea262b2d85aaf09ce1a7a1dc3c2e7aa5
Eric Badger <eric@badgerio.us>
2019-07-15 09:22:04 -0700

    OFI-NCCL: Remove FI_AV_TABLE requirement
    All calls to fi_av_insert() pass a non-NULL fi_addr_t and use that
    returned address for subsequent libfabric calls. It's therefore safe to
    use either AV type, so the av_type attr can be left zeroed (i.e.,
    FI_AV_UNSPEC) to let the provider choose whichever it prefers.

--- src/nccl_ofi_net.c
+++ src/nccl_ofi_net.c
@@ -307,7 +307,6 @@ static int get_ofi_provider(char *prov_include, struct fi_info **prov_info_list)
 
 	hints->ep_attr->type = FI_EP_RDM;
 
-	hints->domain_attr->av_type = FI_AV_TABLE;
 	hints->domain_attr->control_progress = FI_PROGRESS_AUTO;
 	hints->domain_attr->data_progress = FI_PROGRESS_AUTO;
 	/* Indicate that the application support local memory registration */
@@ -463,7 +462,6 @@ static ncclResult_t create_nccl_ofi_component(struct fi_info *prov,
 		goto error;
 	}
 
-	av_attr.type = FI_AV_TABLE;
 	ret = fi_av_open(nccl_ofi_comp->domain, &av_attr, &nccl_ofi_comp->av, NULL);
 	if (OFI_UNLIKELY(ret != 0)) {
 		NCCL_OFI_WARN("Couldn't open AV. RC: %d, ERROR: %s",
