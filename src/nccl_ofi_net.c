fae186a7dd0ae411f919d96ba9c7adbdb00db753
Yongseok Koh <koyongse@amazon.com>
2019-09-23 21:33:45 +0000

    OFI-NCCL: Fix missing completion check for connect API
    Completion is not checked and fetched for the "connect" message to remote
    EP in ofi_connect(). This can causes two issues. a) the message can be
    spoiled before it is really sent as it uses a local stack variable
    (local_ep_addr). b) Some provider such as "sockets" cannot work at all as
    the actual transmission happen when its completion is processed.
    
    Fixes: Potential use after free in ofi_connect() #18 [1]
    
    [1] https://github.com/aws/aws-ofi-nccl/issues/18
    
    Signed-off-by: Yongseok Koh <koyongse@amazon.com>

--- src/nccl_ofi_net.c
+++ src/nccl_ofi_net.c
@@ -1035,6 +1035,11 @@ static ncclResult_t ofi_connect(int dev, void *handle, void **sendComm)
 
 	/* Send "connect" message to remote EP */
 	do {
+		/*
+		 * TODO: replace it with API of FI_INJECT type when most of
+		 * providers can support it, so that need for completion check
+		 * below can be lifted.
+		 */
 		rc = fi_tsend(sComm->local_ep, (void *)&local_ep_addr,
 			      MAX_EP_ADDR, NULL, sComm->remote_ep,
 			      sComm->tag | ~max_tag, &req->ctx);
@@ -1057,6 +1062,13 @@ static ncclResult_t ofi_connect(int dev, void *handle, void **sendComm)
 		}
 	} while (true);
 
+	/* Ensure the message is sent. */
+	do {
+		ret = nccl_ofi_progress(nccl_ofi_component[dev]);
+		if (OFI_UNLIKELY(ret != 0))
+			goto error;
+	} while (req->state != NCCL_OFI_REQ_COMPLETED);
+
 	*sendComm = sComm;
 
 	goto exit;
@@ -1066,9 +1078,9 @@ unlock:
 error:
 	if (sComm)
 		free(sComm);
+exit:
 	if (req)
 		free_nccl_ofi_req(req, false);
-exit:
 	return ret;
 }
 
