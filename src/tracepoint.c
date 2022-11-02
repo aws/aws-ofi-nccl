0e205df6220cec01bbddc17d007e8aaa8439fce7
Ryan Hankins <rqh@amazon.com>
2022-11-02 17:53:03 +0000

    Add LTTNG Tracing to the plugin, including instructions and configuration option.
    Signed-off-by: Ryan Hankins <rqh@amazon.com>

---
+++ src/tracepoint.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022 Amazon.com, Inc. or its affiliates. All rights reserved.
+ */
+
+#include <config.h>
+#if HAVE_LTTNG == 1
+
+#define TRACEPOINT_CREATE_PROBES
+#define LTTNG_UST_TRACEPOINT_DEFINE
+
+/*
+ * tracepoint.c creates the lttng probes, from those created in tracepoint.h.  The probes are created
+ * only if TRACEPOINT_CREATE_PROBES is set before the definitions of those probes, so tracepoint.h must
+ * be included once after that definition.
+ *
+ */
+
+#include <tracepoint.h>
+
+#endif // HAVE_LTTNG == 1
