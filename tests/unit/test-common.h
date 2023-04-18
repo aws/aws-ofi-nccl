9d2dcef5b0a44d6ce83549c3de18cbf16eef41a5
Brian Barrett <bbarrett@amazon.com>
2023-04-18 16:00:15 +0000

    Use Autotools test framework
    Split the tests into two groups: unit tests (which require a single
    process and no runtime) and functional tests (which require multiple
    processes and some type of runtime coordination.  Update the unit
    tests so that they run using the Autotools unit test framework
    (ie, "make check" runs them and fails the build if they do not
    run successfully.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

---
+++ tests/unit/test-common.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018-2023 Amazon.com, Inc. or its affiliates. All rights reserved.
+ */
+
+#ifndef TEST_COMMON_H_
+#define TEST_COMMON_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "nccl_ofi.h"
+#include "nccl_ofi_log.h"
+
+static inline void logger(ncclDebugLogLevel level, unsigned long flags, const char *filefunc,
+			  int line, const char *fmt, ...)
+{
+	va_list vargs;
+
+	switch (level) {
+		case NCCL_LOG_WARN:
+			printf("WARN: Function: %s Line: %d: ", filefunc, line);
+			break;
+		case NCCL_LOG_INFO:
+			printf("INFO: Function: %s Line: %d: ", filefunc, line);
+			break;
+		case NCCL_LOG_TRACE:
+#if OFI_NCCL_TRACE
+			printf("TRACE: Function: %s Line: %d: ", filefunc, line);
+			break;
+#else
+			return;
+#endif
+		default:
+			break;
+	};
+
+	va_start(vargs, fmt);
+	vprintf(fmt, vargs);
+	printf("\n");
+	va_end(vargs);
+}
+
+#endif // End TEST_COMMON_H_
