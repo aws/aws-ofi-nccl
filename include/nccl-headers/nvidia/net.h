32d7f4daf6cb06859b696b925029104d5791d09e
Brian Barrett <bbarrett@amazon.com>
2023-10-04 04:02:19 +0000

    Import NCCL headers from NCCL 2.18.5
    Sync the NCCL headers from the 2.18.5 branch of NCCL.  There is
    only one functional change, removing the ncclInvalidUsage error
    code from the list of valid error codes.  The rest is formatting
    changes, as we had unnecessarily changed the formatting at some
    point in the past.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- include/nccl-headers/nvidia/net.h
+++ include/nccl-headers/nvidia/net.h
@@ -1,14 +1,9 @@
 /*
  * Copyright (c) 2017-2022, NVIDIA CORPORATION. All rights reserved.
- * Copyright (c) 2022 Amazon.com, Inc. or its affiliates. All rights reserved.
  */
 
-#ifndef NCCL_HEADERS_NET_H_
-#define NCCL_HEADERS_NET_H_
-
-#ifdef _cplusplus
-extern "C" {
-#endif
+#ifndef NCCL_NET_H_
+#define NCCL_NET_H_
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -24,28 +19,8 @@ extern "C" {
 // Maximum number of requests per comm object
 #define NCCL_NET_MAX_REQUESTS 8
 
-typedef enum {
-	NCCL_LOG_NONE=0,
-	NCCL_LOG_VERSION=1,
-	NCCL_LOG_WARN=2,
-	NCCL_LOG_INFO=3,
-	NCCL_LOG_ABORT=4,
-	NCCL_LOG_TRACE=5
-} ncclDebugLogLevel;
-
-typedef enum {
-	NCCL_INIT=1,
-	NCCL_COLL=2,
-	NCCL_P2P=4,
-	NCCL_SHM=8,
-	NCCL_NET=16,
-	NCCL_GRAPH=32,
-	NCCL_TUNING=64,
-	NCCL_ENV=128,
-	NCCL_ALLOC=256,
-	NCCL_CALL=512,
-	NCCL_ALL=~0
-} ncclDebugLogSubSys;
+typedef enum {NCCL_LOG_NONE=0, NCCL_LOG_VERSION=1, NCCL_LOG_WARN=2, NCCL_LOG_INFO=3, NCCL_LOG_ABORT=4, NCCL_LOG_TRACE=5} ncclDebugLogLevel;
+typedef enum {NCCL_INIT=1, NCCL_COLL=2, NCCL_P2P=4, NCCL_SHM=8, NCCL_NET=16, NCCL_GRAPH=32, NCCL_TUNING=64, NCCL_ENV=128, NCCL_ALLOC=256, NCCL_CALL=512, NCCL_ALL=~0} ncclDebugLogSubSys;
 
 typedef void (*ncclDebugLogger_t)(ncclDebugLogLevel level, unsigned long flags, const char *file, int line, const char *fmt, ...);
 
@@ -55,10 +30,4 @@ typedef void (*ncclDebugLogger_t)(ncclDebugLogLevel level, unsigned long flags, 
 #include "net_v3.h"
 #include "net_v2.h"
 
-typedef ncclNetProperties_v6_t ncclNetProperties_t;
-
-#ifdef _cplusplus
-} // End extern "C"
-#endif
-
-#endif // End NCCL_NET_H
+#endif // end include guard
