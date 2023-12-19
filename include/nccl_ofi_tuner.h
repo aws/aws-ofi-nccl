eacd69c35ee0440df546d58dfb3e32dbb5064c5d
Raghu Raja <raghunch@amazon.com>
2023-12-19 21:45:54 +0000

    nccl_ofi_tuner: Tuner component for aws-ofi-nccl
    This commit introduces a custom tuner plugin for NCCL that bypasses the
    cost function used by NCCL today. The existing cost function is a
    combination of empirically derived costs, lat/bw reported by network
    plugins, and other coefficients that are unexplainable. These defaults
    seem to work well for IB, but not for EFA. The tuner is currently built
    and used only when running on AWS.
    
    In this initial implementation, a simple Hockney model is used as a
    starting point, and only AllReduce is supported. Once we prove out the
    viability of the tuner plugin in selecting algorithm switch points, we
    can extend this to LogGP-based models to account for protocol specifics
    used in the aws-ofi-nccl plugin (such as the eager->rendezvous switch
    and the cost of control message exhange).
    
    At the time of this commit, NCCL explicitly dlopen()s the
    tuner plugin with a path provided to NCCL_TUNER_PLUGIN, but that will
    change in the future to automatically pick up a tune from the network
    plugin install path (v2.21 likely). NCCL should also look for the tuner
    symbols with dlsym() implicitly if the environment variable is not set,
    but that's a separate discussion to be had with the NCCL team.
    
    Note that nccl-headers/tuner.h had to be slightly modified to remove
    redundancy with definitions in nccl-headers/net.h and
    nccl-headers/error.h. There are still some duplicate definitions, but in
    the interest of keeping the header from NCCL as close to the upstream
    version as possible, this commit does not refactor the common
    definitions for reuse.
    
    Signed-off-by: Raghu Raja <raghunch@amazon.com>

---
+++ include/nccl_ofi_tuner.h
@@ -0,0 +1,106 @@
+#ifndef NCCL_OFI_TUNER_H_
+#define NCCL_OFI_TUNER_H_
+
+#include <linux/limits.h>
+#include <float.h>
+#include "nccl-headers/nvidia/tuner.h"
+#include "nccl_ofi_param.h"
+
+/*
+ * The plugin interface lets us tune the number of channels as well, but that
+ * can come later (once a proto+algo combination is chosen, we can compute the
+ * cost with different channel count and optimize for it.
+ */
+OFI_NCCL_PARAM_INT(tuner_num_channels, "TUNER_NUM_CHANNELS", 8);
+
+/*
+ * Latency in µsecs. Note, this is currently different from the network plugin's param for
+ * net latency by design. When we merge with the platform_data values, we will
+ * need to do some additional testing on the base case where a tuner is not
+ * loaded to make sure the same defaykts make sense across both paths, and
+ * combine the parameters. This parameter is meant for internal testing only and
+ * is not meant to be documented for users.
+ */
+OFI_NCCL_PARAM_INT(tuner_net_latency, "TUNER_NET_LATENCY", 20);
+
+/*
+ * With EFA, we expect a ~2µsec cost in the device and ~1µsec cost to write that
+ * completion up to the host stack.
+ */
+OFI_NCCL_PARAM_INT(tuner_net_comp_overhead, "TUNER_NET_COMP_OVERHEAD", 3);
+
+/* EFA unidirectional network bandwidth */
+#define NCCL_OFI_TUNER_INTERNODE_BW	(12.5 * 1024 * 1024 * 1024 * 1e-6) /* per rail */
+#define NCCL_OFI_TUNER_NET_NUM_RAILS	(4) /* Available to each GPU */
+
+/*
+ * For Hopper GPUs on P5, all intranode communication goes over NVLink, so use
+ * the bandwidth for SM90 architecture in NCCL (SM90_NVLINK_BW).
+ *
+ * This is unidirectional bandwidth per NVLink (900GB/s bidirectional on the
+ * platform, with 18 NVLinks in total. NCCL considers a 20% protocol overhead,
+ * leaving 20GB/s bandwidth per link).
+ *
+ * TODO: When extending to P4/other platforms, include these values in
+ * platform_data and fetch from it for the tuner. Value as defined in Bytes/µsec.
+ */
+#define NCCL_OFI_TUNER_INTRANODE_BW	(20.0 * 1024 * 1024 * 1024 * 1e-6)
+
+/*
+ * NCCL's algo-specific latencies for intra-node cases with NVLink.
+ * The values are directly taken from NCCL (hwLat[])). Values in µsecs.
+ */
+static const float nccl_nvlink_lat[NCCL_NUM_ALGORITHMS][NCCL_NUM_PROTOCOLS] = {
+	{ .6, 1.25,  28 }, /* Tree (LL, LL128, Simple) */
+	{ .6,  1.9, 3.4 }, /* Ring (LL, LL128, Simple) */
+	{  0,    0, 3.7 }, /* Collnet Direct - Unused */
+	{  0,    0, 2.8 }, /* Collnet Chain - Unused */
+	{  0,    0,  23 }, /* NVLS (Simple only) */
+	{  0,    0,  23 }  /* NVLS Tree (Simple only)*/
+};
+
+/*
+ * Base algorithm latencies from NCCL (baseLat[]). For Trees and Rings, NCCL
+ * has empirically derived costs to the algorithms that are otherwise not
+ * captured in the intranode/internode portion of cost computation. This is
+ * considered the base latency, which nccl_ofi_tuner is applying as well. This
+ * can be computed better once we are able to model the pipelining, but adding
+ * these base latencies seem to pick better switchpoints. Values in µsecs.
+ */
+static const float nccl_base_lat[NCCL_NUM_ALGORITHMS][NCCL_NUM_PROTOCOLS] = {
+	{  6.8, 14.0,    0 }, /* Tree */
+	{  6.6, 14.0,  8.4 }, /* Ring */
+	{    0,    0,    0 }, /* Collnet Direct */
+	{    0,    0,    0 }, /* Collnet Chain */
+	{    0,    0,    0 }, /* NVLS */
+	{    0,    0,    0 }  /* NVLS Tree */
+};
+
+struct nccl_ofi_tuner_model_params {
+	float net_lat;
+	float internode_bw;
+	float intranode_bw;
+	int num_rails;
+};
+
+struct nccl_ofi_tuner_context {
+	/* communicator size */
+	int num_ranks;
+	int num_nodes;
+
+	struct nccl_ofi_tuner_model_params model_params;
+
+	float base_costs[NCCL_NUM_FUNCTIONS][NCCL_NUM_ALGORITHMS][NCCL_NUM_PROTOCOLS];
+};
+
+/*
+ * Global context, allocated at _init(). This is allocated and initialized once
+ * per process.
+ */
+struct nccl_ofi_tuner_context *nccl_ofi_tuner_ctx;
+
+/* Modeling functions */
+void nccl_ofi_tuner_model_costs();
+float nccl_ofi_tuner_compute_cost(ncclFunc_t func, int algo, int proto, int pipe_ops, size_t size);
+
+#endif /* NCCL_OFI_TUNER_H_ */
---
+++ src/tuner/nccl_ofi_tuner.c
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include "nccl-headers/nvidia/tuner.h"
+#include "nccl_ofi_tuner.h"
+#include "nccl_ofi_log.h"
+
+pthread_mutex_t nccl_ofi_tuner_ctx_lock = PTHREAD_MUTEX_INITIALIZER;
+
+ncclResult_t nccl_ofi_tuner_init(size_t nRanks, size_t nNodes, ncclDebugLogger_t logFunction)
+{
+	ofi_log_function = logFunction;
+
+	/*
+	 * NCCL parses these variables and applies user filters inside its
+	 * current tuner logic. Ideally, this should be done regardless of the
+	 * use of NCCL's internal tuner or an external tuner plugin. For the
+	 * time being, given the external tuner is an opt-in, detect if a user
+	 * has set one of them and bail when an external tuner is loaded.
+	 */
+	if (getenv("NCCL_ALGO") || getenv("NCCL_PROTO")) {
+		NCCL_OFI_WARN("The tuner plugin can not be loaded when explicitly choosing an algorithm or protocol with NCCL_ALGO/NCCL_PROTO");
+		return ncclInvalidUsage;
+	}
+
+	struct nccl_ofi_tuner_model_params params = {
+		.net_lat = ofi_nccl_tuner_net_latency(),
+		.internode_bw = NCCL_OFI_TUNER_INTERNODE_BW,
+		.intranode_bw = NCCL_OFI_TUNER_INTRANODE_BW,
+		.num_rails = NCCL_OFI_TUNER_NET_NUM_RAILS
+	};
+
+	/*
+	 * The tuner API is missing a mechanism to pass around context after
+	 * initialization. For now, init a plugin-lobal context once.
+	 */ 
+	pthread_mutex_lock(&nccl_ofi_tuner_ctx_lock);
+	if (!nccl_ofi_tuner_ctx) {
+		nccl_ofi_tuner_ctx = calloc(1, sizeof(struct nccl_ofi_tuner_context));
+		if (!nccl_ofi_tuner_ctx) {
+			NCCL_OFI_WARN("Context allocation failed.");
+			return ncclInternalError;
+		}
+
+		nccl_ofi_tuner_ctx->num_ranks = nRanks;
+		nccl_ofi_tuner_ctx->num_nodes = nNodes;
+		nccl_ofi_tuner_ctx->model_params = params;
+
+		/*
+		 * Build cost model to use from nccl_ofi_tuner_get_coll_info.
+		 */
+		nccl_ofi_tuner_model_costs();
+	}
+	pthread_mutex_unlock(&nccl_ofi_tuner_ctx_lock);
+
+	NCCL_OFI_TRACE(NCCL_TUNING, "Tuner init: comm with %ld ranks and %ld nodes.", nRanks, nNodes);
+	return ncclSuccess;
+}
+
+ncclResult_t nccl_ofi_tuner_get_coll_info(ncclFunc_t collType, size_t nBytes,
+				  int collNetSupport, int nvlsSupport, int numPipeOps,
+				  int *algorithm, int *protocol, int* nChannels)
+{
+	float cost = 0;
+	float lowest = FLT_MAX;
+	int algo, proto = 0;
+
+	/* Skip runs smaller than 2 nodes and fallback to NCCL's internal tunings */
+	if (nccl_ofi_tuner_ctx->num_nodes <= 2)
+		return ncclSuccess;
+
+	/*
+	 * Ideally, this should just be a lookup and not be in-flight math
+	 * We do not want divs in the hot path, but working with the API we've
+	 * got now. 
+	 */
+	for (algo = 0; algo < NCCL_NUM_ALGORITHMS; algo++) {
+		/* No CollNet on AWS today */
+		if (algo == NCCL_ALGO_COLLNET_DIRECT || algo == NCCL_ALGO_COLLNET_CHAIN)
+			continue;
+
+		/* Skip NCCL_ALGO_NVLS used only for single-node jobs */
+		if (algo == NCCL_ALGO_NVLS)
+			continue;
+
+		if (!nvlsSupport && (algo == NCCL_ALGO_NVLS_TREE))
+			continue;
+
+		for (proto = 0; proto < NCCL_NUM_PROTOCOLS; proto++) {
+			/* This is not a supported combination in NCCL */
+			if (algo == NCCL_ALGO_NVLS_TREE && proto != NCCL_PROTO_SIMPLE)
+				continue;
+
+			cost = nccl_ofi_tuner_compute_cost(collType, algo, proto, numPipeOps,  nBytes);
+			if (cost < 0)
+				continue;
+
+			NCCL_OFI_TRACE(NCCL_TUNING, "Computed cost for algo %d proto %d pipe %d: cost %.8f µsecs.", algo, proto, numPipeOps, cost);
+			if (cost < lowest) {
+				*algorithm = algo;
+				*protocol = proto;
+				lowest = cost;
+			}
+		}
+	}
+
+	NCCL_OFI_TRACE(NCCL_TUNING, "Choosing algo %d proto %d with cost %.8f µsecs for coll %d size %ld.",
+				    *algorithm, *protocol, lowest, collType, nBytes);
+	return ncclSuccess;
+}
+
+ncclResult_t nccl_ofi_tuner_destroy()
+{
+	free(nccl_ofi_tuner_ctx);
+	return ncclSuccess;
+}
+
+
+const ncclTuner_v1_t ncclTunerPlugin_v1 = {
+  .name = "nccl_ofi_tuner",
+  .init = nccl_ofi_tuner_init,
+  .getCollInfo = nccl_ofi_tuner_get_coll_info,
+  .destroy = nccl_ofi_tuner_destroy
+};
