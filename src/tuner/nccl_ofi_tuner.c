cbe2c7815da9a6966bf9a54dc3265f5ffb0fe7e1
Raghu Raja <raghunch@amazon.com>
2024-02-05 22:31:57 +0000

    tuner: Use a separate logger for tuner
    NCCL dlopen()s ext-net plugins and tuner plugins with `RTLD_LOCAL`, so the
    symbols from loading libnccl-net.so is not available to the tuner by
    default. Declaring a separate logger for the tuner while being able to
    reuse the logging macros addresses this issue without having to add in
    the entirety of nccl_ofi_api.c objects into the tuner.
    
    This properly addresses the issue that was patched with 64482e8.
    
    Signed-off-by: Raghu Raja <raghunch@amazon.com>

--- src/tuner/nccl_ofi_tuner.c
+++ src/tuner/nccl_ofi_tuner.c
@@ -6,6 +6,7 @@
 
 struct nccl_ofi_tuner_context *nccl_ofi_tuner_ctx;
 pthread_mutex_t nccl_ofi_tuner_ctx_lock = PTHREAD_MUTEX_INITIALIZER;
+ncclDebugLogger_t ofi_log_function = NULL;
 
 ncclResult_t nccl_ofi_tuner_init(size_t nRanks, size_t nNodes, ncclDebugLogger_t logFunction)
 {
