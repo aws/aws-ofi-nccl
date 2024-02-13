4c002b45ce8d9e3f1c6f7484c514d887960d66fd
Liran Alon <liran@amazon.com>
2024-02-14 01:21:23 +0200

    nccl-headers: Introduce include/nccl-headers/types.h nccl-header
    Commit bdc047940493 ("Import NCCL headers from NCCL 2.18.5") introduced
    include/nccl-headers/nvidia/types.h header file but did not provide a
    wrapper header that allows the rest of the code to include it.
    
    Expose a header that allows to do so. Similar to
    include/nccl-headers/error.h.
    
    Signed-off-by: Liran Alon <liran@amazon.com>

---
+++ include/nccl-headers/types.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c)      2024 Amazon.com, Inc. or its affiliates. All rights reserved.
+ */
+
+#ifndef NCCL_HEADERS_TYPES_H
+#define NCCL_HEADERS_TYPES_H
+
+#if HAVE_CUDA
+#include "nccl-headers/nvidia/types.h"
+#endif
+
+#endif
