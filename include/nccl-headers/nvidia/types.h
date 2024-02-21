aws/aws-ofi-nccl

commit bdc047940493529941203bd4fe1ff867e1ea9244

Import NCCL headers from NCCL 2.18.5
Sync the NCCL headers from the 2.18.5 branch of NCCL.  There is
only one functional change, removing the ncclInvalidUsage error
code from the list of valid error codes.  The rest is formatting
changes, as we had unnecessarily changed the formatting at some
point in the past.

Signed-off-by: Brian Barrett <bbarrett@amazon.com>

include/nccl-headers/nvidia/types.h
@@ -1,0 +1,21 @@
+/*
+ * Copyright (c) 2017-2022, NVIDIA CORPORATION. All rights reserved.
+ */
+
+#ifndef NCCL_ERR_H_
+#define NCCL_ERR_H_
+
+/* Data types */
+typedef enum { ncclInt8       = 0, ncclChar       = 0,
+               ncclUint8      = 1,
+               ncclInt32      = 2, ncclInt        = 2,
+               ncclUint32     = 3,
+               ncclInt64      = 4,
+               ncclUint64     = 5,
+               ncclFloat16    = 6, ncclHalf       = 6,
+               ncclFloat32    = 7, ncclFloat      = 7,
+               ncclFloat64    = 8, ncclDouble     = 8,
+               ncclBfloat16   = 9,
+} ncclDataType_t;
+
+#endif

include/nccl-headers/nvidia/err.h
@@ -1,28 +1,16 @@
 /*
  * Copyright (c) 2017-2022, NVIDIA CORPORATION. All rights reserved.
- * Copyright (c) 2022 Amazon.com, Inc. or its affiliates. All rights reserved.
  */
 
-#ifndef ERROR_H_
-#define ERROR_H_
+#ifndef NCCL_ERR_H_
+#define NCCL_ERR_H_
 
-#ifdef _cplusplus
-extern "C" {
+/* Error type for plugins */
+typedef enum { ncclSuccess                 =  0,
+               ncclUnhandledCudaError      =  1,
+               ncclSystemError             =  2,
+               ncclInternalError           =  3,
+               ncclInvalidArgument         =  4,
+               ncclRemoteError             =  6 } ncclResult_t;
+
 #endif
-
-/* NCCL error type for plugins. These are similar to the ones present in nccl.h.in */
-typedef enum {
-	ncclSuccess =  0,
-	ncclUnhandledCudaError = 1,
-	ncclSystemError = 2,
-	ncclInternalError = 3,
-	ncclInvalidArgument = 4,
-	ncclInvalidUsage = 5,
-	ncclRemoteError = 6
-} ncclResult_t;
-
-#ifdef _cplusplus
-}
-#endif
-
-#endif // End ERROR_H_

include/nccl-headers/nvidia/net.h
@@ -1,14 +1,9 @@
 /*
  * Copyright (c) 2017-2022, NVIDIA CORPORATION. All rights reserved.
- * Copyright (c) 2022 Amazon.com, Inc. or its affiliates. All rights reserved.
  */
 
-#ifndef NCCL_HEADERS_NET_H_
-#define NCCL_HEADERS_NET_H_
-
-#ifdef _cplusplus
-extern "C" {
-#endif
+#ifndef NCCL_NET_H_
+#define NCCL_NET_H_
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -24,28 +19,8 @@ extern "C" {
 // Maximum number of requests per comm object
 #define NCCL_NET_MAX_REQUESTS 8
 
-typedef enum {
-	NCCL_LOG_NONE=0,
-	NCCL_LOG_VERSION=1,
-	NCCL_LOG_WARN=2,
-	NCCL_LOG_INFO=3,
-	NCCL_LOG_ABORT=4,
-	NCCL_LOG_TRACE=5
-} ncclDebugLogLevel;
-
-typedef enum {
-	NCCL_INIT=1,
-	NCCL_COLL=2,
-	NCCL_P2P=4,
-	NCCL_SHM=8,
-	NCCL_NET=16,
-	NCCL_GRAPH=32,
-	NCCL_TUNING=64,
-	NCCL_ENV=128,
-	NCCL_ALLOC=256,
-	NCCL_CALL=512,
-	NCCL_ALL=~0
-} ncclDebugLogSubSys;
+typedef enum {NCCL_LOG_NONE=0, NCCL_LOG_VERSION=1, NCCL_LOG_WARN=2, NCCL_LOG_INFO=3, NCCL_LOG_ABORT=4, NCCL_LOG_TRACE=5} ncclDebugLogLevel;
+typedef enum {NCCL_INIT=1, NCCL_COLL=2, NCCL_P2P=4, NCCL_SHM=8, NCCL_NET=16, NCCL_GRAPH=32, NCCL_TUNING=64, NCCL_ENV=128, NCCL_ALLOC=256, NCCL_CALL=512, NCCL_ALL=~0} ncclDebugLogSubSys;
 
 typedef void (*ncclDebugLogger_t)(ncclDebugLogLevel level, unsigned long flags, const char *file, int line, const char *fmt, ...);
 
@@ -55,10 +30,4 @@ typedef void (*ncclDebugLogger_t)(ncclDebugLogLevel level, unsigned long flags, 
 #include "net_v3.h"
 #include "net_v2.h"
 
-typedef ncclNetProperties_v6_t ncclNetProperties_t;
-
-#ifdef _cplusplus
-} // End extern "C"
-#endif
-
-#endif // End NCCL_NET_H
+#endif // end include guard

include/nccl-headers/nvidia/net_v2.h
@@ -1,59 +1,50 @@
 /*
  * Copyright (c) 2017-2022, NVIDIA CORPORATION. All rights reserved.
- * Copyright (c) 2022 Amazon.com, Inc. or its affiliates. All rights reserved.
  */
 
-#ifndef NCCL_HEADERS_NET_V2_H_
-#define NCCL_HEADERS_NET_V2_H_
-
-#ifdef _cplusplus
-extern "C" {
-#endif
+#ifndef NCCL_NET_V2_H_
+#define NCCL_NET_V2_H_
 
 typedef struct {
-	// Name of the network (mainly for logs)
-	const char* name;
-	// Initialize the network.
-	ncclResult_t (*init)(ncclDebugLogger_t logFunction);
-	// Return the number of adapters.
-	ncclResult_t (*devices)(int* ndev);
-	// Return the device path in /sys. NCCL will call free on this path.
-	ncclResult_t (*pciPath)(int dev, char** path);
-	// Return whether this device supports host pointers and/or CUDA pointers
-	// as data from the current GPU. Supported types should be composed with
-	// NCCL_PTR_HOST and NCCL_PTR_CUDA.
-	ncclResult_t (*ptrSupport)(int dev, int* supportedTypes);
-	// Create a receiving object and provide a handle to connect to it. The
-	// handle can be up to NCCL_NET_HANDLE_MAXSIZE bytes and will be exchanged
-	// between ranks to create a connection.
-	ncclResult_t (*listen)(int dev, void* handle, void** listenComm);
-	// Connect to a handle and return a sending comm object for that peer.
-	ncclResult_t (*connect)(int dev, void* handle, void** sendComm);
-	// Finalize connection establishment after remote peer has called connectHandle
-	ncclResult_t (*accept)(void* listenComm, void** recvComm);
-	// Register/Deregister memory. Comm can be either a sendComm or a recvComm.
-	ncclResult_t (*regMr)(void* comm, void* data, int size, int type, void** mhandle);
-	ncclResult_t (*deregMr)(void* comm, void* mhandle);
-	// Asynchronous send to a peer. Type is either NCCL_PTR_HOST or NCCL_PTR_CUDA.
-	// May return request == NULL if the call cannot be performed (or would block)
-	ncclResult_t (*isend)(void* sendComm, void* data, int size, void* mhandle, void** request);
-	// Asynchronous recv from a peer. Type is either NCCL_PTR_HOST or NCCL_PTR_CUDA.
-	// May return request == NULL if the call cannot be performed (or would block)
-	ncclResult_t (*irecv)(void* recvComm, void* data, int size, void* mhandle, void** request);
-	// Perform a flush/fence to make sure all data received with NCCL_PTR_CUDA is
-	// visible to the GPU
-	ncclResult_t (*flush)(void* recvComm, void* data, int size, void* mhandle);
-	// Test whether a request is complete. If size is not NULL, it returns the
-	// number of bytes sent/received.
-	ncclResult_t (*test)(void* request, int* done, int* size);
-	// Close and free send/recv comm objects
-	ncclResult_t (*closeSend)(void* sendComm);
-	ncclResult_t (*closeRecv)(void* recvComm);
-	ncclResult_t (*closeListen)(void* listenComm);
+  // Name of the network (mainly for logs)
+  const char* name;
+  // Initialize the network.
+  ncclResult_t (*init)(ncclDebugLogger_t logFunction);
+  // Return the number of adapters.
+  ncclResult_t (*devices)(int* ndev);
+  // Return the device path in /sys. NCCL will call free on this path.
+  ncclResult_t (*pciPath)(int dev, char** path);
+  // Return whether this device supports host pointers and/or CUDA pointers
+  // as data from the current GPU. Supported types should be composed with
+  // NCCL_PTR_HOST and NCCL_PTR_CUDA.
+  ncclResult_t (*ptrSupport)(int dev, int* supportedTypes);
+  // Create a receiving object and provide a handle to connect to it. The
+  // handle can be up to NCCL_NET_HANDLE_MAXSIZE bytes and will be exchanged
+  // between ranks to create a connection.
+  ncclResult_t (*listen)(int dev, void* handle, void** listenComm);
+  // Connect to a handle and return a sending comm object for that peer.
+  ncclResult_t (*connect)(int dev, void* handle, void** sendComm);
+  // Finalize connection establishment after remote peer has called connectHandle
+  ncclResult_t (*accept)(void* listenComm, void** recvComm);
+  // Register/Deregister memory. Comm can be either a sendComm or a recvComm.
+  ncclResult_t (*regMr)(void* comm, void* data, int size, int type, void** mhandle);
+  ncclResult_t (*deregMr)(void* comm, void* mhandle);
+  // Asynchronous send to a peer. Type is either NCCL_PTR_HOST or NCCL_PTR_CUDA.
+  // May return request == NULL if the call cannot be performed (or would block)
+  ncclResult_t (*isend)(void* sendComm, void* data, int size, void* mhandle, void** request);
+  // Asynchronous recv from a peer. Type is either NCCL_PTR_HOST or NCCL_PTR_CUDA.
+  // May return request == NULL if the call cannot be performed (or would block)
+  ncclResult_t (*irecv)(void* recvComm, void* data, int size, void* mhandle, void** request);
+  // Perform a flush/fence to make sure all data received with NCCL_PTR_CUDA is
+  // visible to the GPU
+  ncclResult_t (*flush)(void* recvComm, void* data, int size, void* mhandle);
+  // Test whether a request is complete. If size is not NULL, it returns the
+  // number of bytes sent/received.
+  ncclResult_t (*test)(void* request, int* done, int* size);
+  // Close and free send/recv comm objects
+  ncclResult_t (*closeSend)(void* sendComm);
+  ncclResult_t (*closeRecv)(void* recvComm);
+  ncclResult_t (*closeListen)(void* listenComm);
 } ncclNet_v2_t;
 
-#ifdef _cplusplus
-}
-#endif // End extern "C"
-
-#endif // End NCCL_HEADERS_NET_V2_H_
+#endif // end include guard

include/nccl-headers/nvidia/net_v3.h
@@ -1,59 +1,50 @@
 /*
  * Copyright (c) 2017-2022, NVIDIA CORPORATION. All rights reserved.
- * Copyright (c) 2022 Amazon.com, Inc. or its affiliates. All rights reserved.
  */
 
-#ifndef NCCL_HEADERS_NET_V3_H_
-#define NCCL_HEADERS_NET_V3_H_
-
-#ifdef _cplusplus
-extern "C" {
-#endif
+#ifndef NCCL_NET_V3_H_
+#define NCCL_NET_V3_H_
 
 #define NCCL_NET_MAX_REQUESTS_V3 16
 
 typedef ncclNetProperties_v4_t ncclNetProperties_v3_t;
 typedef struct {
-	// Name of the network (mainly for logs)
-	const char* name;
-	// Initialize the network.
-	ncclResult_t (*init)(ncclDebugLogger_t logFunction);
-	// Return the number of adapters.
-	ncclResult_t (*devices)(int* ndev);
-	// Get various device properties.
-	ncclResult_t (*getProperties)(int dev, ncclNetProperties_v3_t* props);
-	// Create a receiving object and provide a handle to connect to it. The
-	// handle can be up to NCCL_NET_HANDLE_MAXSIZE bytes and will be exchanged
-	// between ranks to create a connection.
-	ncclResult_t (*listen)(int dev, void* handle, void** listenComm);
-	// Connect to a handle and return a sending comm object for that peer.
-	ncclResult_t (*connect)(int dev, void* handle, void** sendComm);
-	// Finalize connection establishment after remote peer has called connectHandle
-	ncclResult_t (*accept)(void* listenComm, void** recvComm);
-	// Register/Deregister memory. Comm can be either a sendComm or a recvComm.
-	// Type is either NCCL_PTR_HOST or NCCL_PTR_CUDA.
-	ncclResult_t (*regMr)(void* comm, void* data, int size, int type, void** mhandle);
-	ncclResult_t (*deregMr)(void* comm, void* mhandle);
-	// Asynchronous send to a peer.
-	// May return request == NULL if the call cannot be performed (or would block)
-	ncclResult_t (*isend)(void* sendComm, void* data, int size, void* mhandle, void** request);
-	// Asynchronous recv from a peer.
-	// May return request == NULL if the call cannot be performed (or would block)
-	ncclResult_t (*irecv)(void* recvComm, void* data, int size, void* mhandle, void** request);
-	// Perform a flush/fence to make sure all data received with NCCL_PTR_CUDA is
-	// visible to the GPU
-	ncclResult_t (*flush)(void* recvComm, void* data, int size, void* mhandle);
-	// Test whether a request is complete. If size is not NULL, it returns the
-	// number of bytes sent/received.
-	ncclResult_t (*test)(void* request, int* done, int* size);
-	// Close and free send/recv comm objects
-	ncclResult_t (*closeSend)(void* sendComm);
-	ncclResult_t (*closeRecv)(void* recvComm);
-	ncclResult_t (*closeListen)(void* listenComm);
+  // Name of the network (mainly for logs)
+  const char* name;
+  // Initialize the network.
+  ncclResult_t (*init)(ncclDebugLogger_t logFunction);
+  // Return the number of adapters.
+  ncclResult_t (*devices)(int* ndev);
+  // Get various device properties.
+  ncclResult_t (*getProperties)(int dev, ncclNetProperties_v3_t* props);
+  // Create a receiving object and provide a handle to connect to it. The
+  // handle can be up to NCCL_NET_HANDLE_MAXSIZE bytes and will be exchanged
+  // between ranks to create a connection.
+  ncclResult_t (*listen)(int dev, void* handle, void** listenComm);
+  // Connect to a handle and return a sending comm object for that peer.
+  ncclResult_t (*connect)(int dev, void* handle, void** sendComm);
+  // Finalize connection establishment after remote peer has called connectHandle
+  ncclResult_t (*accept)(void* listenComm, void** recvComm);
+  // Register/Deregister memory. Comm can be either a sendComm or a recvComm.
+  // Type is either NCCL_PTR_HOST or NCCL_PTR_CUDA.
+  ncclResult_t (*regMr)(void* comm, void* data, int size, int type, void** mhandle);
+  ncclResult_t (*deregMr)(void* comm, void* mhandle);
+  // Asynchronous send to a peer.
+  // May return request == NULL if the call cannot be performed (or would block)
+  ncclResult_t (*isend)(void* sendComm, void* data, int size, void* mhandle, void** request);
+  // Asynchronous recv from a peer.
+  // May return request == NULL if the call cannot be performed (or would block)
+  ncclResult_t (*irecv)(void* recvComm, void* data, int size, void* mhandle, void** request);
+  // Perform a flush/fence to make sure all data received with NCCL_PTR_CUDA is
+  // visible to the GPU
+  ncclResult_t (*flush)(void* recvComm, void* data, int size, void* mhandle);
+  // Test whether a request is complete. If size is not NULL, it returns the
+  // number of bytes sent/received.
+  ncclResult_t (*test)(void* request, int* done, int* size);
+  // Close and free send/recv comm objects
+  ncclResult_t (*closeSend)(void* sendComm);
+  ncclResult_t (*closeRecv)(void* recvComm);
+  ncclResult_t (*closeListen)(void* listenComm);
 } ncclNet_v3_t;
 
-#ifdef _cplusplus
-}
-#endif // End extern "C"
-
-#endif // End NCCL_HEADERS_NET_V3_H_
+#endif // end include guard

include/nccl-headers/nvidia/net_v4.h
@@ -1,74 +1,61 @@
 /*
  * Copyright (c) 2017-2022, NVIDIA CORPORATION. All rights reserved.
- * Copyright (c) 2022 Amazon.com, Inc. or its affiliates. All rights reserved.
  */
 
-#ifndef NCCL_HEADERS_NET_V4_H_
-#define NCCL_HEADERS_NET_V4_H_
-
-#ifdef _cplusplus
-extern "C" {
-#endif
+#ifndef NCCL_NET_V4_H_
+#define NCCL_NET_V4_H_
 
 #define NCCL_NET_HANDLE_MAXSIZE_V4 64
 
 typedef struct {
-	char* name;     // Used mostly for logging.
-	char* pciPath;  // Path to the PCI device in /sys.
-	uint64_t guid;  // Unique identifier for the NIC chip. Important for
-			// cards with multiple PCI functions (Physical or virtual).
-	int ptrSupport; // NCCL_PTR_HOST or NCCL_PTR_HOST|NCCL_PTR_CUDA
-	int speed;      // Port speed in Mbps.
-	int port;       // Port number.
-	int maxComms;   // Maximum number of comms we can create
+  char* name;     // Used mostly for logging.
+  char* pciPath;  // Path to the PCI device in /sys.
+  uint64_t guid;  // Unique identifier for the NIC chip. Important for
+                  // cards with multiple PCI functions (Physical or virtual).
+  int ptrSupport; // NCCL_PTR_HOST or NCCL_PTR_HOST|NCCL_PTR_CUDA
+  int speed;      // Port speed in Mbps.
+  int port;       // Port number.
+  int maxComms;   // Maximum number of comms we can create
 } ncclNetProperties_v4_t;
 
 // v4 struct for backwards compatibility
 typedef struct {
-	// Name of the network (mainly for logs)
-	const char* name;
-	// Initialize the network.
-	ncclResult_t (*init)(ncclDebugLogger_t logFunction);
-	// Return the number of adapters.
-	ncclResult_t (*devices)(int* ndev);
-	// Get various device properties.
-	ncclResult_t (*getProperties)(int dev, ncclNetProperties_v4_t* props);
-	// Create a receiving object and provide a handle to connect to it. The
-	// handle can be up to NCCL_NET_HANDLE_MAXSIZE bytes and will be exchanged
-	// between ranks to create a connection.
-	ncclResult_t (*listen)(int dev, void* handle, void** listenComm);
-	// Connect to a handle and return a sending comm object for that peer.
-	ncclResult_t (*connect)(int dev, void* handle, void** sendComm);
-	// Finalize connection establishment after remote peer has called connectHandle
-	ncclResult_t (*accept)(void* listenComm, void** recvComm);
-	// Register/Deregister memory. Comm can be either a sendComm or a recvComm.
-	// Type is either NCCL_PTR_HOST or NCCL_PTR_CUDA.
-#if HAVE_NEURON
-	ncclResult_t (*regMr)(void* comm, void* data, size_t size, int type, void** mhandle);
-#elif HAVE_CUDA
-	ncclResult_t (*regMr)(void* comm, void* data, int size, int type, void** mhandle);
-#endif
-	ncclResult_t (*deregMr)(void* comm, void* mhandle);
-	// Asynchronous send to a peer.
-	// May return request == NULL if the call cannot be performed (or would block)
-	ncclResult_t (*isend)(void* sendComm, void* data, int size, void* mhandle, void** request);
-	// Asynchronous recv from a peer.
-	// May return request == NULL if the call cannot be performed (or would block)
-	ncclResult_t (*irecv)(void* recvComm, void* data, int size, void* mhandle, void** request);
-	// Perform a flush/fence to make sure all data received with NCCL_PTR_CUDA is
-	// visible to the GPU
-	ncclResult_t (*iflush)(void* recvComm, void* data, int size, void* mhandle, void** request);
-	// Test whether a request is complete. If size is not NULL, it returns the
-	// number of bytes sent/received.
-	ncclResult_t (*test)(void* request, int* done, int* size);
-	// Close and free send/recv comm objects
-	ncclResult_t (*closeSend)(void* sendComm);
-	ncclResult_t (*closeRecv)(void* recvComm);
-	ncclResult_t (*closeListen)(void* listenComm);
+  // Name of the network (mainly for logs)
+  const char* name;
+  // Initialize the network.
+  ncclResult_t (*init)(ncclDebugLogger_t logFunction);
+  // Return the number of adapters.
+  ncclResult_t (*devices)(int* ndev);
+  // Get various device properties.
+  ncclResult_t (*getProperties)(int dev, ncclNetProperties_v4_t* props);
+  // Create a receiving object and provide a handle to connect to it. The
+  // handle can be up to NCCL_NET_HANDLE_MAXSIZE bytes and will be exchanged
+  // between ranks to create a connection.
+  ncclResult_t (*listen)(int dev, void* handle, void** listenComm);
+  // Connect to a handle and return a sending comm object for that peer.
+  ncclResult_t (*connect)(int dev, void* handle, void** sendComm);
+  // Finalize connection establishment after remote peer has called connectHandle
+  ncclResult_t (*accept)(void* listenComm, void** recvComm);
+  // Register/Deregister memory. Comm can be either a sendComm or a recvComm.
+  // Type is either NCCL_PTR_HOST or NCCL_PTR_CUDA.
+  ncclResult_t (*regMr)(void* comm, void* data, int size, int type, void** mhandle);
+  ncclResult_t (*deregMr)(void* comm, void* mhandle);
+  // Asynchronous send to a peer.
+  // May return request == NULL if the call cannot be performed (or would block)
+  ncclResult_t (*isend)(void* sendComm, void* data, int size, void* mhandle, void** request);
+  // Asynchronous recv from a peer.
+  // May return request == NULL if the call cannot be performed (or would block)
+  ncclResult_t (*irecv)(void* recvComm, void* data, int size, void* mhandle, void** request);
+  // Perform a flush/fence to make sure all data received with NCCL_PTR_CUDA is
+  // visible to the GPU
+  ncclResult_t (*iflush)(void* recvComm, void* data, int size, void* mhandle, void** request);
+  // Test whether a request is complete. If size is not NULL, it returns the
+  // number of bytes sent/received.
+  ncclResult_t (*test)(void* request, int* done, int* size);
+  // Close and free send/recv comm objects
+  ncclResult_t (*closeSend)(void* sendComm);
+  ncclResult_t (*closeRecv)(void* recvComm);
+  ncclResult_t (*closeListen)(void* listenComm);
 } ncclNet_v4_t;
 
-#ifdef _cplusplus
-}
-#endif // End extern "C"
-
-#endif // End NCCL_HEADERS_NET_V4_H_
+#endif // end include guard

include/nccl-headers/nvidia/net_v5.h
@@ -1,63 +1,54 @@
 /*
  * Copyright (c) 2017-2022, NVIDIA CORPORATION. All rights reserved.
- * Copyright (c) 2022 Amazon.com, Inc. or its affiliates. All rights reserved.
  */
 
-#ifndef NCCL_HEADERS_NET_V5_H_
-#define NCCL_HEADERS_NET_V5_H_
-
-#ifdef _cplusplus
-extern "C" {
-#endif
+#ifndef NCCL_NET_V5_H_
+#define NCCL_NET_V5_H_
 
 typedef ncclNetProperties_v6_t ncclNetProperties_v5_t;
 typedef struct {
-	// Name of the network (mainly for logs)
-	const char* name;
-	// Initialize the network.
-	ncclResult_t (*init)(ncclDebugLogger_t logFunction);
-	// Return the number of adapters.
-	ncclResult_t (*devices)(int* ndev);
-	// Get various device properties.
-	ncclResult_t (*getProperties)(int dev, ncclNetProperties_v5_t* props);
-	// Create a receiving object and provide a handle to connect to it. The
-	// handle can be up to NCCL_NET_HANDLE_MAXSIZE bytes and will be exchanged
-	// between ranks to create a connection.
-	ncclResult_t (*listen)(int dev, void* handle, void** listenComm);
-	// Connect to a handle and return a sending comm object for that peer.
-	// This call must not block for the connection to be established, and instead
-	// should return successfully with sendComm == NULL with the expectation that
-	// it will be called again until sendComm != NULL.
-	ncclResult_t (*connect)(int dev, void* handle, void** sendComm);
-	// Finalize connection establishment after remote peer has called connect.
-	// This call must not block for the connection to be established, and instead
-	// should return successfully with recvComm == NULL with the expectation that
-	// it will be called again until recvComm != NULL.
-	ncclResult_t (*accept)(void* listenComm, void** recvComm);
-	// Register/Deregister memory. Comm can be either a sendComm or a recvComm.
-	// Type is either NCCL_PTR_HOST or NCCL_PTR_CUDA.
-	ncclResult_t (*regMr)(void* comm, void* data, int size, int type, void** mhandle);
-	ncclResult_t (*deregMr)(void* comm, void* mhandle);
-	// Asynchronous send to a peer.
-	// May return request == NULL if the call cannot be performed (or would block)
-	ncclResult_t (*isend)(void* sendComm, void* data, int size, int tag, void* mhandle, void** request);
-	// Asynchronous recv from a peer.
-	// May return request == NULL if the call cannot be performed (or would block)
-	ncclResult_t (*irecv)(void* recvComm, int n, void** data, int* sizes, int* tags, void** mhandles, void** request);
-	// Perform a flush/fence to make sure all data received with NCCL_PTR_CUDA is
-	// visible to the GPU
-	ncclResult_t (*iflush)(void* recvComm, int n, void** data, int* sizes, void** mhandles, void** request);
-	// Test whether a request is complete. If size is not NULL, it returns the
-	// number of bytes sent/received.
-	ncclResult_t (*test)(void* request, int* done, int* sizes);
-	// Close and free send/recv comm objects
-	ncclResult_t (*closeSend)(void* sendComm);
-	ncclResult_t (*closeRecv)(void* recvComm);
-	ncclResult_t (*closeListen)(void* listenComm);
+  // Name of the network (mainly for logs)
+  const char* name;
+  // Initialize the network.
+  ncclResult_t (*init)(ncclDebugLogger_t logFunction);
+  // Return the number of adapters.
+  ncclResult_t (*devices)(int* ndev);
+  // Get various device properties.
+  ncclResult_t (*getProperties)(int dev, ncclNetProperties_v5_t* props);
+  // Create a receiving object and provide a handle to connect to it. The
+  // handle can be up to NCCL_NET_HANDLE_MAXSIZE bytes and will be exchanged
+  // between ranks to create a connection.
+  ncclResult_t (*listen)(int dev, void* handle, void** listenComm);
+  // Connect to a handle and return a sending comm object for that peer.
+  // This call must not block for the connection to be established, and instead
+  // should return successfully with sendComm == NULL with the expectation that
+  // it will be called again until sendComm != NULL.
+  ncclResult_t (*connect)(int dev, void* handle, void** sendComm);
+  // Finalize connection establishment after remote peer has called connect.
+  // This call must not block for the connection to be established, and instead
+  // should return successfully with recvComm == NULL with the expectation that
+  // it will be called again until recvComm != NULL.
+  ncclResult_t (*accept)(void* listenComm, void** recvComm);
+  // Register/Deregister memory. Comm can be either a sendComm or a recvComm.
+  // Type is either NCCL_PTR_HOST or NCCL_PTR_CUDA.
+  ncclResult_t (*regMr)(void* comm, void* data, int size, int type, void** mhandle);
+  ncclResult_t (*deregMr)(void* comm, void* mhandle);
+  // Asynchronous send to a peer.
+  // May return request == NULL if the call cannot be performed (or would block)
+  ncclResult_t (*isend)(void* sendComm, void* data, int size, int tag, void* mhandle, void** request);
+  // Asynchronous recv from a peer.
+  // May return request == NULL if the call cannot be performed (or would block)
+  ncclResult_t (*irecv)(void* recvComm, int n, void** data, int* sizes, int* tags, void** mhandles, void** request);
+  // Perform a flush/fence to make sure all data received with NCCL_PTR_CUDA is
+  // visible to the GPU
+  ncclResult_t (*iflush)(void* recvComm, int n, void** data, int* sizes, void** mhandles, void** request);
+  // Test whether a request is complete. If size is not NULL, it returns the
+  // number of bytes sent/received.
+  ncclResult_t (*test)(void* request, int* done, int* sizes);
+  // Close and free send/recv comm objects
+  ncclResult_t (*closeSend)(void* sendComm);
+  ncclResult_t (*closeRecv)(void* recvComm);
+  ncclResult_t (*closeListen)(void* listenComm);
 } ncclNet_v5_t;
 
-#ifdef _cplusplus
-}
-#endif // End extern "C"
-
-#endif // End NCCL_HEADERS_NET_V5_H_
+#endif // end include guard

commit aeeb20957320690400694f3e6d18c57203b3d1de

nccl-headers: Fix wrong types.h header guard macro
NCCL_ERR_H_ is used as include/nccl-headers/nvidia/err.h header guard.
Therefore, types.h should use a different header guard name.

This wrong guard name was actually a bug copied as is from NCCL
types.h header file.

Fixes: bdc047940493 ("Import NCCL headers from NCCL 2.18.5")
Signed-off-by: Liran Alon <liran@amazon.com>

include/nccl-headers/nvidia/types.h
@@ -2,8 +2,8 @@
  * Copyright (c) 2017-2022, NVIDIA CORPORATION. All rights reserved.
  */
 
-#ifndef NCCL_ERR_H_
-#define NCCL_ERR_H_
+#ifndef NCCL_TYPES_H_
+#define NCCL_TYPES_H_
 
 /* Data types */
 typedef enum { ncclInt8       = 0, ncclChar       = 0,