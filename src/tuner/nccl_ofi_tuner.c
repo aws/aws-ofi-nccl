2adb8f56553fe0946141180c4a119c96eca766d8
Nicholas Sielicki <nslick@amazon.com>
2024-03-11 12:28:55 -0700

    fix(build): tuner: prefer ncclInvalidArgument
    ncclInvalidArgument is present both in neuron and nvidia whereas
    ncclInvalidUsage is only present in neuron headers. Callsite just wraps
    the value in ncclcheck and the meaning isn't meaningfully changed, so
    prefer the portable one.

--- src/tuner/nccl_ofi_tuner.c
+++ src/tuner/nccl_ofi_tuner.c
@@ -21,7 +21,12 @@ ncclResult_t nccl_ofi_tuner_init(size_t nRanks, size_t nNodes, ncclDebugLogger_t
 	 */
 	if (getenv("NCCL_ALGO") || getenv("NCCL_PROTO")) {
 		NCCL_OFI_WARN("The tuner plugin can not be loaded when explicitly choosing an algorithm or protocol with NCCL_ALGO/NCCL_PROTO");
-		return ncclInvalidUsage;
+		// FIXME: "ncclInvalidUsage should be returned when the error is
+		// most likely a user error" per nccl docs, which arguably makes
+		// it a better return code here than ncclInvalidArgument, but
+		// the former is currently not vended in ext-net headers, so
+		// we're returning ncclInvalidArgument instead.
+		return ncclInvalidArgument;
 	}
 
 	struct nccl_ofi_tuner_model_params params = {
