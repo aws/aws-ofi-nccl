3d4b9cbf890ef7738a13bd068a0d39fc3aaebcb3
Nicholas Sielicki <nslick@amazon.com>
2024-05-13 10:59:07 -0700

    fix: lttng: fix include define
    Signed-off-by: Nicholas Sielicki <nslick@amazon.com>
    (cherry picked from commit 8158c2b242460a89dd83cbf378aaf0e0a828e810)

--- include/tracing_impl/lttng.h
+++ include/tracing_impl/lttng.h
@@ -6,7 +6,7 @@
 #define LTTNG_UST_TRACEPOINT_PROVIDER nccl_ofi_plugin
 
 #undef LTTNG_UST_TRACEPOINT_INCLUDE
-#define LTTNG_UST_TRACEPOINT_INCLUDE "include/tracing_impl/lttng.h"
+#define LTTNG_UST_TRACEPOINT_INCLUDE "tracing_impl/lttng.h"
 
 /*
  * To add a tracepoint at the nccl_ofi_plugin layer:
