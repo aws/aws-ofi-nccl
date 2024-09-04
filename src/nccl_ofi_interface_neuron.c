b87011feb0663e38a5115dda4b3b19bad74d1e8a
Nicholas Sielicki <nslick@amazon.com>
2024-09-04 13:55:58 -0700

    fix(neuron): remove const from ncclNetPlugin_v{4,5} syms (#577)
    const variables have internal linkage by default under c++; nvidia
    interface requires it not be const because its modified during init.
    Remove const for neuron, too.
    
    Signed-off-by: Nicholas Sielicki <nslick@amazon.com>

--- src/nccl_ofi_interface_neuron.c
+++ src/nccl_ofi_interface_neuron.c
@@ -83,24 +83,24 @@ static ncclResult_t accept_v5(void* listenComm, void** recvComm)
 	return nccl_net_ofi_accept(listenComm, recvComm);
 }
 
-NCCL_OFI_EXPORT_SYMBOL const ncclNet_v5_t ncclNetPlugin_v5 = {
-        .name = "AWS Libfabric",
-        .init = nccl_net_ofi_init,
-        .devices = nccl_net_ofi_devices,
-        .getProperties = getProperties_v5,
-        .listen = nccl_net_ofi_listen,
-        .connect = connect_v5,
-        .accept = accept_v5,
-        .regMr = nccl_net_ofi_regMr,
-        .regMrDmaBuf = nccl_net_ofi_regMrDmaBuf,
-        .deregMr = nccl_net_ofi_deregMr,
-        .isend = nccl_net_ofi_isend,
-        .irecv = nccl_net_ofi_irecv,
-        .iflush = nccl_net_ofi_iflush,
-        .test = nccl_net_ofi_test,
-        .closeSend = nccl_net_ofi_closeSend,
-        .closeRecv = nccl_net_ofi_closeRecv,
-        .closeListen = nccl_net_ofi_closeListen,
+NCCL_OFI_EXPORT_SYMBOL ncclNet_v5_t ncclNetPlugin_v5 = {
+	.name = "AWS Libfabric",
+	.init = nccl_net_ofi_init,
+	.devices = nccl_net_ofi_devices,
+	.getProperties = getProperties_v5,
+	.listen = nccl_net_ofi_listen,
+	.connect = connect_v5,
+	.accept = accept_v5,
+	.regMr = nccl_net_ofi_regMr,
+	.regMrDmaBuf = nccl_net_ofi_regMrDmaBuf,
+	.deregMr = nccl_net_ofi_deregMr,
+	.isend = nccl_net_ofi_isend,
+	.irecv = nccl_net_ofi_irecv,
+	.iflush = nccl_net_ofi_iflush,
+	.test = nccl_net_ofi_test,
+	.closeSend = nccl_net_ofi_closeSend,
+	.closeRecv = nccl_net_ofi_closeRecv,
+	.closeListen = nccl_net_ofi_closeListen,
 	.getMrKey = nccl_net_ofi_get_mr_key,
 	.iwrite = nccl_net_ofi_iwrite,
 	.iwriteInline = nccl_net_ofi_iwrite_inline,
@@ -129,7 +129,7 @@ static ncclResult_t getProperties_v4(int dev_id, ncclNetProperties_v4_t *props)
 	return ncclSuccess;
 }
 
-NCCL_OFI_EXPORT_SYMBOL const ncclNet_v4_t ncclNetPlugin_v4 = {
+NCCL_OFI_EXPORT_SYMBOL ncclNet_v4_t ncclNetPlugin_v4 = {
 	.name = "AWS Libfabric",
 	.init = init_v4,
 	.devices = nccl_net_ofi_devices,
