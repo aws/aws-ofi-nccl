6dec51bfce5e1f8f7d430ff4307cccc986c39b68
Amedeo Sapio <asapio@amazon.com>
2024-09-10 01:51:33 +0000

    fix(tuner): don't choose NVLSTree if nRanks==nNodes
    When nRanks==nNodes (SPLIT_MASK=0x7 in NCCL-tests) NCCL is telling the tuner
    that NVLS is not supported, because it is only doing inter node communication
    and NVLS doesn't works across nodes.
    So the region that chooses NVLSTree+Simple is ignored by the tuner, and it
    falls back to NCCL's tuner. The best choice there is Ring+Simple, so this is
    updating that region.
    
    Signed-off-by: Amedeo Sapio <asapio@amazon.com>

--- src/tuner/nccl_ofi_tuner.c
+++ src/tuner/nccl_ofi_tuner.c
@@ -269,7 +269,7 @@ ncclResult_t nccl_ofi_tuner_init(size_t nRanks, size_t nNodes, ncclDebugLogger_t
 				      {9999360, 64},
 				      {2367488, 16},
 				      {0, 16}}},
-			{.algorithm = NCCL_ALGO_NVLS_TREE,
+			{.algorithm = NCCL_ALGO_RING,
 			 .protocol = NCCL_PROTO_SIMPLE,
 			 .num_vertices = 4,
 			 .vertices = {{4736000, 2}, {TUNER_MAX_SIZE, 2}, extended_ring_ll128, {269484032, 128}}}};
