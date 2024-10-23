4333ea7c93099cea28a215fe15427c3756fd7a4a
Brian Barrett <bbarrett@amazon.com>
2024-10-23 21:36:21 +0000

    rdma: Fix rail reordering inconsistency
    The combination of rail reordering when creating a device that uses
    multiple rails and topology generation created a bug that could lead
    to performance issues or crashes in NCCL.  The autogenerated topology
    file only included the "leader NICs", which were the first provider
    in the info list.  This reordering logic happened *after* topology
    generation, but before getProperties(), which returns the BDF of the
    first info (ie leader NIC).  If NICs got reordered, the leader NIC
    could be inconsistent between those two reporting points, which
    irritates NCCL.  This has not been a problem historically, because
    we either used a static topology file that included all NICs (plugin
    1.10 or earlier) *or* we didn't reorder.  It's only recently that EFA
    started providing enough information to reorder and trip the above
    bug.
    
    The fix is to move the reordering logic into topology generation, where
    it arguably should have been from the start.  Now there's no possibility
    of inconsistent views, because the single source of truth is always
    used.
    
    This patch leaves the group count unhandled by the AWS implementation
    of the sorter.  This is not a problem on existing platforms, and will
    be fixed in a follow-on commit.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- include/nccl_ofi_platform.h
+++ include/nccl_ofi_platform.h
@@ -27,13 +27,31 @@ int platform_init(const char **provider_filter) __attribute__((weak));
  */
 int platform_config_endpoint(struct fi_info *info, struct fid_ep *ep) __attribute__((weak));
 
-/* Platform-specific hook to sort in the multi-rail protocol of the plugin. Some
- * providers rely on having a consistent ordering of rail indices for best
- * performance.
+/* Platform-specific hook to sort in the multi-rail protocol of the
+ * plugin
+ *
+ * Rail-oriented networks or traffic flows are a common performance
+ * optimization for ML networks.  Generally, Libfabric providers sort
+ * their provider list by BDFs, which are indicitive of physical
+ * ordering and good enough.  However, on some platforms (especially
+ * virtualized platforms), this might not actually be sufficient and
+ * another sorting mechanism may be required to properly group NICs.
+ *
+ * This interface is called in the topology initialization code to
+ * order NICs that are behind the same PCIe root complex / switch.
+ * The info_list will have num_rails providers listed, and will later
+ * be split into num_groups groups (based on the number of
+ * accelerators that are also behind the PCIe switch).
+ *
+ * Providers of this interface should sort the provided info_list such
+ * that the Nth provider on this node will be assumed to talk to the
+ * Nth provider on remote nodes (ie, identify the "rail id" and sort
+ * by that).
+ *
  * @param info_list: pointer to list of `num_rails` info objects
  * @param num_rails: number of rails
  */
-void platform_sort_rails(struct fi_info **info_list, int num_rails) __attribute__((weak));
+void platform_sort_rails(struct fi_info **info_list, int num_rails, size_t num_groups) __attribute__((weak));
 
 
 /*
