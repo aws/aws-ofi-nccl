8ee095ed7598008e74a11da16497f965095f64ab
Brian Barrett <bbarrett@amazon.com>
2025-03-05 19:11:56 +0000

    rdma: Remove ofi domain pointer from endpoint
    The fid_domain pointer in the endpoint is a remnant of the world before
    we had a domain pointer.  We can easily get the domain object anywhere
    we need the fid_domain, so remove the endpoint fid_domain.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- include/nccl_ofi_rdma.h
+++ include/nccl_ofi_rdma.h
@@ -676,9 +676,6 @@ struct nccl_net_ofi_ep_rail {
 	/* Completion Queue handle */
 	struct fid_cq *cq;
 
-	/* Access domain handles */
-	struct fid_domain *domain;
-
 	/*
 	 * Rx buffer management
 	 */
