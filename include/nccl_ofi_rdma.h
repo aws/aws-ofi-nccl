945e77857727c0de092b27004fd9341535fbdf90
Brian Barrett <bbarrett@amazon.com>
2025-03-18 22:22:22 +0000

    rdma: Only poll to completion  of tested req
    Rather than polling the completion queues until they're empty, stop
    polling on request completion or finding the control message.  This
    should (in theory) lower latency of any given polling loop.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- include/nccl_ofi_rdma.h
+++ include/nccl_ofi_rdma.h
@@ -1222,6 +1222,9 @@ public:
 	 * @return	0, on success
 	 *		error, on others
 	 */
+	template <typename func>
+	int ofi_process_cq(func done_check);
+
 	int ofi_process_cq();
 
 	int handle_rx_eagain(nccl_net_ofi_rdma_ep_rail_t *rail,
