5521877d941a606291239ec7e70ce36e8a109f41
Brian Barrett <bbarrett@amazon.com>
2025-03-25 17:50:09 +0000

    rdma: Split control and eager posted rx limits
    Split the handling of control and eager posted rx buffer min/max count.
    Commit ff8e4c9 split the buffer pools for eager and control messages,
    removing one of the last pieces of code tying the two buffer types
    together.  This patch removes the last piece, limiting the rx buffer
    counts to the same limits on control and data rx lists.  With the
    split, we also increase the number of control messages posted, to
    avoid RNR situations in all-to-all like patterns.  Because control
    messages are so small (~500 bytes), increasing the number of them
    significantly does not significantly increase host memory utilization.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

--- include/nccl_ofi_param.h
+++ include/nccl_ofi_param.h
@@ -283,20 +283,42 @@ OFI_NCCL_PARAM_INT(disable_dmabuf, "DISABLE_DMABUF", 1);
 OFI_NCCL_PARAM_UINT(min_stripe_size, "MIN_STRIPE_SIZE", (128 * 1024));
 
 /*
- * Minimum rx buffers (ctrl/eager) posted per endpoint. The plugin will attempt
- * to post more rx buffers if we dip below this threshold, allocating new rx
- * buffers if needed.
- *
- * Note: the parameter is called "bounce buffer" for backward compatibility.
+ * Deprecated value to control both eager and control bounce counts.
  */
-OFI_NCCL_PARAM_INT(rdma_min_posted_bounce_buffers, "RDMA_MIN_POSTED_BOUNCE_BUFFERS", 64);
+OFI_NCCL_PARAM_INT(deprecated_rdma_min_posted_bounce_buffers, "RDMA_MIN_POSTED_BOUNCE_BUFFERS", -1);
+
+/*
+ * Deprecated value to control both eager and control bounce counts.
+ */
+OFI_NCCL_PARAM_INT(deprecated_rdma_max_posted_bounce_buffers, "RDMA_MAX_POSTED_BOUNCE_BUFFERS", -1);
+
+/*
+ * Minimum eager rx buffers posted per endpoint. The plugin will attempt to post
+ * more rx buffers if we dip below this threshold, allocating new rx buffers if
+ * needed.
+ */
+OFI_NCCL_PARAM_INT(rdma_min_posted_eager_buffers, "RDMA_MIN_POSTED_EAGER_BUFFERS", 64);
 
 /*
  * Maximum rx buffers posted per endpoint. The plugin will not attempt to
  * post more rx buffers if we reach this threshold, returning available
  * buffers to the free list if needed
  */
-OFI_NCCL_PARAM_INT(rdma_max_posted_bounce_buffers, "RDMA_MAX_POSTED_BOUNCE_BUFFERS", 128);
+OFI_NCCL_PARAM_INT(rdma_max_posted_eager_buffers, "RDMA_MAX_POSTED_EAGER_BUFFERS", 128);
+
+/*
+ * Minimum control rx buffers posted per endpoint. The plugin will attempt to post
+ * more rx buffers if we dip below this threshold, allocating new rx buffers if
+ * needed.
+ */
+OFI_NCCL_PARAM_INT(rdma_min_posted_control_buffers, "RDMA_MIN_POSTED_CONTROL_BUFFERS", 1920);
+
+/*
+ * Maximum rx buffers posted per endpoint. The plugin will not attempt to
+ * post more rx buffers if we reach this threshold, returning available
+ * buffers to the free list if needed
+ */
+OFI_NCCL_PARAM_INT(rdma_max_posted_control_buffers, "RDMA_MAX_POSTED_CONTROL_BUFFERS", 2048);
 
 /*
  * Whether to spread the control message across multiple rails in round robin fashion or
