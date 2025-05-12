d66ba96347b22d4ae348dcb5a05da3d821f755d6
Eric Raut <eraut@amazon.com>
2025-05-12 16:22:47 -0700

    rdma, sendrecv: deactivate domain when closing inflight communicators
    Closing a communicator that still has requests inflight will deactivate
    the domain, preventing use of all other communicators on the domain for
    anything except closing the communicator.
    
    This is necessary because, when closing a communicator, the only way to
    ensure user memory is no longer accessed is to close the corresponding
    endpoint.
    
    Signed-off-by: Eric Raut <eraut@amazon.com>

--- src/nccl_ofi_sendrecv.cpp
+++ src/nccl_ofi_sendrecv.cpp
@@ -1190,6 +1190,35 @@ static int sendrecv_recv_comm_recv(nccl_net_ofi_recv_comm_t *recv_comm, int n, v
 	return ret;
 }
 
+
+/**
+ * Abort an endpoint when a communicator using it still has inflight requests
+ *
+ * This function will
+ * 1. Close the OFI resources (ep, av) associated with the endpoint
+ * 2. Mark the associated domain as inactive to prevent further use of domain
+ *    resources, such as completion queue
+ *
+ * After this function returns, the endpoint will still have non-OFI resources
+ * allocated (freelists, rx requests, etc.), but will not be usable except to
+ * release it (release_ep).
+ */
+static inline void sendrecv_endpoint_abort(nccl_net_ofi_sendrecv_ep_t *ep)
+{
+	nccl_net_ofi_domain_t *base_domain = &sendrecv_endpoint_get_domain(ep)->base;
+
+	pthread_wrapper domain_lock(&base_domain->domain_lock);
+
+	int dev_id = base_domain->device->dev_id;
+
+	nccl_ofi_ofiutils_ep_release(ep->ofi_ep, ep->av, dev_id);
+	ep->ofi_ep = NULL;
+	ep->av = NULL;
+
+	base_domain->domain_active = false;
+}
+
+
 static int sendrecv_recv_comm_close(nccl_net_ofi_recv_comm_t *recv_comm)
 {
 	nccl_net_ofi_sendrecv_recv_comm_t *r_comm =
@@ -1198,11 +1227,22 @@ static int sendrecv_recv_comm_close(nccl_net_ofi_recv_comm_t *recv_comm)
 	nccl_net_ofi_sendrecv_mr_handle_t *mr_handle = nullptr;
 
 	/* Retrieve and validate endpoint */
-	nccl_net_ofi_ep_t *base_ep = r_comm->base.base.ep;
-	if (OFI_UNLIKELY(base_ep == NULL)) {
+	auto *ep = reinterpret_cast<nccl_net_ofi_sendrecv_ep_t *>(r_comm->base.base.ep);
+	if (OFI_UNLIKELY(ep == NULL)) {
 		ret = -EINVAL;
 		NCCL_OFI_WARN("Invalid endpoint provided");
-		goto exit;
+		return ret;
+	}
+
+	nccl_net_ofi_ep_t *base_ep = &ep->base;
+
+	/* If there are still requests in-flight, we need to also close the
+	 * endpoint and invalidate the domain */
+	if (r_comm->num_inflight_reqs > 0) {
+		NCCL_OFI_WARN("Closing recv_comm %p with inflight requests. Invalidating domain",
+			      r_comm);
+
+		sendrecv_endpoint_abort(ep);
 	}
 
 	if (!ofi_nccl_gdr_flush_disable() && support_gdr == GDR_SUPPORTED && !cuda_flush) {
@@ -2001,11 +2041,22 @@ static int sendrecv_send_comm_close(nccl_net_ofi_send_comm_t *send_comm)
 	int ret = 0;
 
 	/* Retrieve and validate endpoint */
-	nccl_net_ofi_ep_t *base_ep = s_comm->base.base.ep;
-	if (OFI_UNLIKELY(base_ep == NULL)) {
+	auto *ep = reinterpret_cast<nccl_net_ofi_sendrecv_ep_t *>(s_comm->base.base.ep);
+	if (OFI_UNLIKELY(ep == NULL)) {
 		ret = -EINVAL;
 		NCCL_OFI_WARN("Invalid endpoint provided");
-		goto exit;
+		return ret;
+	}
+
+	nccl_net_ofi_ep_t *base_ep = &ep->base;
+
+	/* If there are still requests in-flight, we need to also close the
+	 * endpoint and invalidate the domain */
+	if (s_comm->num_inflight_reqs > 0) {
+		NCCL_OFI_WARN("Closing send_comm %p with inflight requests. Invalidating domain",
+			      s_comm);
+
+		sendrecv_endpoint_abort(ep);
 	}
 
 	nccl_ofi_freelist_fini(s_comm->nccl_ofi_reqs_fl);
@@ -2016,7 +2067,7 @@ static int sendrecv_send_comm_close(nccl_net_ofi_send_comm_t *send_comm)
 	free(send_comm);
 
 	ret = base_ep->release_ep(base_ep, false, false);
- exit:
+
 	return ret;
 }
 
