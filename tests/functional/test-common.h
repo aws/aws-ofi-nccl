f46937b6dc2c50c1385e8ac09f24154f20a9373a
Hershel Shah <hershys@amazon.com>
2025-10-30 03:30:02 +0000

    utils: Adding buffer and memory related helper functions
    Adding other common buffer and memory related helper functions to the
    functional common functional test header. The multi-threaded functional
    tests will heavily use these functions.
    
    Signed-off-by: Hershel Shah <hershys@amazon.com>

--- tests/functional/test-common.h
+++ tests/functional/test-common.h
@@ -119,15 +119,25 @@ static inline int is_gdr_supported_nic(uint64_t ptr_support)
 	return 0;
 }
 
+/**
+ * Allocate buffer (host or CUDA device memory)
+ *
+ * @param buf Output pointer to allocated buffer
+ * @param size Size of buffer in bytes
+ * @param buffer_type NCCL_PTR_HOST for host memory, NCCL_PTR_CUDA for device memory
+ * @return ncclSuccess on success, error code otherwise
+ */
 static inline ncclResult_t allocate_buff(void **buf, size_t size, int buffer_type)
 {
 	switch (buffer_type) {
-	case NCCL_PTR_CUDA:
-		NCCL_OFI_TRACE(NCCL_NET, "Allocating CUDA buffer");
-		CUDACHECK(cudaMalloc(buf, size));
+	case NCCL_PTR_CUDA: {
+		// HACK: Round up to nearest 4KB to prevent running into mr_cache
+		// bug on small unaligned memory allocations
+		size_t aligned_size = ((size + 4095) / 4096) * 4096;
+		CUDACHECK(cudaMalloc(buf, aligned_size));
 		break;
+	}
 	case NCCL_PTR_HOST:
-		NCCL_OFI_TRACE(NCCL_NET, "Allocating host buffer");
 		CUDACHECK(cudaHostAlloc(buf, size, cudaHostAllocMapped));
 		break;
 	default:
@@ -138,6 +148,14 @@ static inline ncclResult_t allocate_buff(void **buf, size_t size, int buffer_typ
 	return ncclSuccess;
 }
 
+/**
+ * Initialize buffer with a pattern
+ *
+ * @param buf Buffer to initialize
+ * @param size Size of buffer in bytes
+ * @param buffer_type NCCL_PTR_HOST for host memory, NCCL_PTR_CUDA for device memory
+ * @return ncclSuccess on success, error code otherwise
+ */
 static inline ncclResult_t initialize_buff(void *buf, size_t size, int buffer_type)
 {
 	switch (buffer_type) {
@@ -155,6 +173,13 @@ static inline ncclResult_t initialize_buff(void *buf, size_t size, int buffer_ty
 	return ncclSuccess;
 }
 
+/**
+ * Deallocate buffer
+ *
+ * @param buf Buffer to deallocate
+ * @param buffer_type NCCL_PTR_HOST for host memory, NCCL_PTR_CUDA for device memory
+ * @return ncclSuccess on success, error code otherwise
+ */
 static inline ncclResult_t deallocate_buffer(void *buf, int buffer_type)
 {
 	switch (buffer_type) {
@@ -172,6 +197,15 @@ static inline ncclResult_t deallocate_buffer(void *buf, int buffer_type)
 	return ncclSuccess;
 }
 
+/**
+ * Validate received data against expected data
+ *
+ * @param recv_buf Received buffer to validate
+ * @param expected_buf Expected buffer to compare against
+ * @param size Size of buffers in bytes
+ * @param buffer_type NCCL_PTR_HOST for host memory, NCCL_PTR_CUDA for device memory
+ * @return ncclSuccess if data matches, error code otherwise
+ */
 static inline ncclResult_t validate_data(char *recv_buf, char *expected_buf, size_t size, int buffer_type)
 {
 	int ret = 0;
@@ -202,6 +236,62 @@ static inline ncclResult_t validate_data(char *recv_buf, char *expected_buf, siz
 		return ncclInvalidArgument;
 	}
 
+	return ncclSuccess;
+}
+
+
+/**
+ * Register memory with communicator
+ *
+ * Registers a memory region with a communicator for RDMA operations.
+ * Calls the plugin's regMr function to obtain a memory handle.
+ *
+ * @param ext_net Pointer to external plugin
+ * @param comm Communicator to register memory with
+ * @param buffer Pointer to buffer to register
+ * @param size Size of buffer in bytes
+ * @param buffer_type NCCL_PTR_HOST for host memory, NCCL_PTR_CUDA for device memory
+ * @param mhandle Output pointer to memory handle
+ * @return ncclSuccess on success, error code otherwise
+ */
+inline ncclResult_t register_memory(test_nccl_net_t* ext_net, void* comm, void* buffer, size_t size, int buffer_type, void** mhandle)
+{
+	NCCL_OFI_TRACE(NCCL_NET, "Registering memory: buffer=%p, size=%zu, type=%d",
+	               buffer, size, buffer_type);
+	*mhandle = nullptr;
+
+	// Call plugin's regMr function
+	OFINCCLCHECK(ext_net->regMr(comm, buffer, size, buffer_type, mhandle));
+
+	NCCL_OFI_TRACE(NCCL_NET, "Memory registered successfully: mhandle=%p", *mhandle);
+	return ncclSuccess;
+}
+
+/**
+ * Deregister memory from communicator
+ *
+ * Deregisters a previously registered memory region. Calls the plugin's
+ * deregMr function to release the memory handle.
+ *
+ * @param ext_net Pointer to plugin interface
+ * @param comm Communicator to deregister memory from
+ * @param mhandle Memory handle to deregister
+ * @return ncclSuccess on success, error code otherwise
+ */
+static inline ncclResult_t deregister_memory(test_nccl_net_t* ext_net, void* comm, void* mhandle)
+{
+	// Deregistering NULL handle is a no-op, not an error
+	if (mhandle == nullptr) {
+		NCCL_OFI_TRACE(NCCL_NET, "Skipping deregistration of NULL memory handle");
+		return ncclSuccess;
+	}
+
+	NCCL_OFI_TRACE(NCCL_NET, "Deregistering memory: mhandle=%p", mhandle);
+
+	// Call plugin's deregMr function
+	OFINCCLCHECK(ext_net->deregMr(comm, mhandle));
+
+	NCCL_OFI_TRACE(NCCL_NET, "Memory deregistered successfully");
 	return ncclSuccess;
 }
 
