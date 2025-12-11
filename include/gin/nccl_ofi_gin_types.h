d9274f541053457fca6c697bb5c543b276ee3dd7
Eric Raut <eraut@amazon.com>
2025-12-10 16:07:11 -0800

    gin: Add endpoint and local MR types
    Signed-off-by: Eric Raut <eraut@amazon.com>

---
+++ include/gin/nccl_ofi_gin_types.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026      Amazon.com, Inc. or its affiliates. All rights reserved.
+ */
+
+#ifndef NCCL_OFI_GIN_TYPES_H
+#define NCCL_OFI_GIN_TYPES_H
+
+/**
+ * Forward-declarations of GIN types
+ */
+class nccl_ofi_gin_mr_handle_t;
+
+#endif
