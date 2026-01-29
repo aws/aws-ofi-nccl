5700aeebfac475da0fb04b0f02a43d92f910c03e
Brian Barrett <bbarrett@amazon.com>
2026-01-29 20:29:17 +0000

    Add minimal spinlock interface
    Add a very minimal spinlock implementation that can be used as a way of
    having barrier-less release semantics on x86 for the endpoint lock.
    
    Signed-off-by: Brian Barrett <bbarrett@amazon.com>

---
+++ include/nccl_ofi_spinlock.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2026      Amazon.com, Inc. or its affiliates. All rights reserved.
+ */
+
+#ifndef NET_OFI_SPINLOCK_H_
+#define NET_OFI_SPINLOCK_H_
+
+#include <atomic>
+
+
+// A BasicLockable spinlock without many features
+class CAPABILITY("mutex") nccl_ofi_spinlock {
+public	:
+	nccl_ofi_spinlock() : val(false)
+	{
+		std::atomic_thread_fence(std::memory_order_release);
+	}
+
+	nccl_ofi_spinlock(const nccl_ofi_spinlock&) = delete;
+	nccl_ofi_spinlock& operator=(const nccl_ofi_spinlock&) = delete;
+
+	bool trylock() TRY_ACQUIRE(true)
+	{
+		lock_type::value_type old = false;
+		return val.compare_exchange_strong(old, true, std::memory_order_acquire,
+						   std::memory_order_relaxed);
+	}
+
+
+	void lock() ACQUIRE()
+	{
+		while (!this->trylock()) {
+			while (val.load()) {
+#if defined(__x86_64__)
+				asm volatile("pause" : : : );
+#elif defined(__aarch64__)
+				asm volatile("isb" : : : );
+#endif
+			}
+		}
+	}
+
+
+	void unlock() RELEASE()
+	{
+		val.store(false, std::memory_order_release);
+	}
+
+
+private:
+	using lock_type = std::atomic<bool>;
+
+	lock_type val;
+};
+
+#endif
