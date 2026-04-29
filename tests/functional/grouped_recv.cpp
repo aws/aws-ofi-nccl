6dd7f6d8085850db46a39c9e3ff7fc26ee1f1b52
Amit Radzi <amitrad@amazon.com>
2026-04-29 18:29:07 +0000

    tests: add mixed-size grouped receive functional tests
    Add GroupedRecvMixedSizeTest that sends different sizes per
    sub-receive (1024*(i+1) bytes) and validates that test() reports
    the actual per-sub size, not an even division of the total.
    
    Add two test cases: n=2 and n=4 mixed sizes.
    
    Signed-off-by: Amit Radzi <amitrad@amazon.com>
    (cherry picked from commit 2878c23cae94a4ad0c037b559ce4fc766c0748fc)

--- tests/functional/grouped_recv.cpp
+++ tests/functional/grouped_recv.cpp
@@ -134,6 +134,113 @@ private:
 	}
 };
 
+class GroupedRecvMixedSizeTest : public TestScenario {
+public:
+	GroupedRecvMixedSizeTest(int num_recvs)
+		: TestScenario("Grouped Recv Mixed Sizes (n=" + std::to_string(num_recvs) + ")"),
+		  n_recvs(num_recvs) {}
+
+	void run(ThreadContext& ctx) override {
+		test_nccl_properties_t props = {};
+		OFINCCLTHROW(ext_net->getProperties(0, &props));
+
+		if (props.maxRecvs < n_recvs) {
+			NCCL_OFI_INFO(NCCL_NET,
+				"Skipping: maxRecvs=%d < n_recvs=%d", props.maxRecvs, n_recvs);
+			return;
+		}
+
+		for (size_t dev_idx = 0; dev_idx < ctx.lcomms.size(); dev_idx++) {
+			mixed_send_receive(ctx, dev_idx);
+		}
+	}
+
+private:
+	int n_recvs;
+	static constexpr int TAG = 1;
+
+	/* Each sub-recv gets a different size: 1024*(i+1) */
+	size_t sub_size(int i) { return 1024 * (i + 1); }
+
+	void mixed_send_receive(ThreadContext& ctx, int dev_idx) {
+		void *sComm = ctx.scomms[dev_idx];
+		void *rComm = ctx.rcomms[dev_idx];
+
+		auto gdr_support = get_support_gdr(ext_net);
+		int buffer_type = gdr_support[dev_idx] ? NCCL_PTR_CUDA : NCCL_PTR_HOST;
+
+		if (ctx.rank == 0) {
+			void* send_bufs[MAX_RECVS] = {};
+			void* send_mh[MAX_RECVS] = {};
+			void* send_reqs[MAX_RECVS] = {};
+
+			for (int i = 0; i < n_recvs; i++) {
+				size_t sz = sub_size(i);
+				OFINCCLTHROW(allocate_buff(&send_bufs[i], sz, buffer_type));
+				OFINCCLTHROW(initialize_buff(send_bufs[i], sz, buffer_type, 'A' + i));
+				OFINCCLTHROW(ext_net->regMr(sComm, send_bufs[i], sz, buffer_type, &send_mh[i]));
+				post_send(ext_net, sComm, send_bufs[i], sz, TAG, send_mh[i], &send_reqs[i]);
+			}
+
+			bool all_done = false;
+			while (!all_done) {
+				all_done = true;
+				for (int i = 0; i < n_recvs; i++) {
+					if (send_reqs[i]) {
+						int done = 0;
+						OFINCCLTHROW(ext_net->test(send_reqs[i], &done, nullptr));
+						if (done) send_reqs[i] = nullptr;
+						else all_done = false;
+					}
+				}
+			}
+
+			for (int i = 0; i < n_recvs; i++) {
+				ext_net->deregMr(sComm, send_mh[i]);
+				deallocate_buffer(send_bufs[i], buffer_type);
+			}
+		} else {
+			void* recv_bufs[MAX_RECVS] = {};
+			void* recv_mh[MAX_RECVS] = {};
+			size_t sizes[MAX_RECVS];
+			int tags[MAX_RECVS];
+			void* request = nullptr;
+
+			/* Allocate each sub-recv with its own size */
+			for (int i = 0; i < n_recvs; i++) {
+				size_t sz = sub_size(i);
+				OFINCCLTHROW(allocate_buff(&recv_bufs[i], sz, buffer_type));
+				OFINCCLTHROW(ext_net->regMr(rComm, recv_bufs[i], sz, buffer_type, &recv_mh[i]));
+				sizes[i] = sz;
+				tags[i] = TAG;
+			}
+
+			post_recv(ext_net, rComm, n_recvs, recv_bufs, sizes, tags, recv_mh, &request);
+
+			int done = 0;
+			int recv_sizes[MAX_RECVS] = {};
+			while (!done) {
+				OFINCCLTHROW(ext_net->test(request, &done, recv_sizes));
+			}
+
+			/* Validate per-sub sizes match actual sent sizes */
+			for (int i = 0; i < n_recvs; i++) {
+				size_t expected_sz = sub_size(i);
+				if (recv_sizes[i] != (int)expected_sz) {
+					throw std::runtime_error(
+						"Sub-recv " + std::to_string(i) +
+						": expected size " + std::to_string(expected_sz) +
+						" got " + std::to_string(recv_sizes[i]));
+				}
+			}
+
+			for (int i = 0; i < n_recvs; i++) {
+				ext_net->deregMr(rComm, recv_mh[i]);
+				deallocate_buffer(recv_bufs[i], buffer_type);
+			}
+		}
+	}
+};
 int main(int argc, char* argv[])
 {
 	/* Disable eager to avoid 2-iovec sends with mixed host/device memory */
@@ -157,5 +264,12 @@ int main(int argc, char* argv[])
 	suite.add(&test_n8);
 	suite.add(&test_n8_large);
 
+	/* Mixed sizes: each sub-recv has a different size */
+	GroupedRecvMixedSizeTest test_mixed_n2(2);
+	GroupedRecvMixedSizeTest test_mixed_n4(4);
+	suite.add(&test_mixed_n2);
+	suite.add(&test_mixed_n4);
+
 	return suite.run_all();
 }
+
